8a20ed921bfa0ace762076a739566f1dbc456639
Eljah Capers <elicasper2000@yahoo.com>
2024-02-22 11:49:28 -0500

    Completed Lab Feb 22

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+// function findMatching(collection, attribute){
+//     const matchingDrivers =[];
+//     for (const user of collection){
+//         // const param1 = collection[user].toLowerCase();
+//         // const param2 = attribute.toLowerCase();
+//         if (user === attribute){
+//             matchingDrivers.push(user)
+//         }
+//     }
+//     return matchingDrivers
+// }
+
+function findMatching(collection, attribute){
+    return collection.filter(function(collection){
+        return collection.toLowerCase() === attribute.toLowerCase()
+    })
+}
+
+function fuzzyMatch(collection, attribute){
+    return collection.filter(function(collection){
+        return collection.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()
+    })
+}
+
+function matchName(collection, attribute){
+    return collection.filter(function(user){
+        return user.name === attribute
+    })
+}
